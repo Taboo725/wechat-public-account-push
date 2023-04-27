0e194a87adcb96d920ac71010989baf7adea9ec5
Taboo725 <76696223+Taboo725@users.noreply.github.com>
2023-04-27 17:27:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -68,7 +68,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oBZrn6FZdUMs5j-HeqsFRZm46IaA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'J-TNrfH_tGsJlOdPSST7byjdcoqP9iAtsRw-7-PMKUE',
+      useTemplateId: 'hwoYG_UGh3yUSjIlRXZfyn4cZ4w2rFwh4VMZI9r74qY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
