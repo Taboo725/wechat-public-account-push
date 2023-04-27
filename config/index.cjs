7ea04e340d0282649da0a8898bad2801c490dffd
Taboo725 <76696223+Taboo725@users.noreply.github.com>
2023-04-27 16:43:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
   
-  USE_PASSAGE: 'push-deer',
+  // USE_PASSAGE: 'push-deer',
   
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx1f3eb59e5b30f273',
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU22261THOijjIGdhf3ljSmSgsyb6QIRFfJArzC1',
+      id: 'oBZrn6BD9jHPdjbzWEb0ikoBmv3c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'BvvQdp9xNY9_BhOCgdrP69ArrAdumTynVhzkJ24oi4A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-27',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '	JwTW9eckirOpS1YL8o6hQtsKvwY5ZYz-1Ti6GoRPxL8',
 
   CALLBACK_USERS: [
     {
