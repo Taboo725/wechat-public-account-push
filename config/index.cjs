ff1cd92b13c9105181a18917e44a75ecb16741a3
Taboo725 <76696223+Taboo725@users.noreply.github.com>
2023-04-27 17:07:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -71,6 +71,8 @@ const USER_CONFIG = {
       useTemplateId: 'BvvQdp9xNY9_BhOCgdrP69ArrAdumTynVhzkJ24oi4A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-27',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
